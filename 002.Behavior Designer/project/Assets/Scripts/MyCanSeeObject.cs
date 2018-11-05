c38d0f4f0b4aacbcdfc4fb2c8360878f1270dd1d
Everglow <30494065+DengHaiYang1234@users.noreply.github.com>
2018-11-05 22:38:12 +0800

    2018.11.5
    行为树  手写

---
+++ 002.Behavior Designer/project/Assets/Scripts/MyCanSeeObject.cs
@@ -0,0 +1,38 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using UnityEngine;
+
+public class MyCanSeeObject : Conditional
+{
+    public Transform[] targets;
+    public float fieldOfViewAngle = 90;
+    public float viewDistance = 7;
+
+    public SharedTransform target;
+
+    public override TaskStatus OnUpdate()
+    {
+        if (targets == null)
+            return TaskStatus.Failure;
+
+        foreach (var target in targets)
+        {
+            float distance = (target.position - transform.position).magnitude;
+            //两个向量的夹角 z向量与目标向量的夹角
+            float angle = Vector3.Angle(transform.forward, target.position - transform.position);
+
+            //目标与z轴的夹角要小于  可是角度的一半
+            if (distance < viewDistance && angle < fieldOfViewAngle*0.5f)
+            {
+                this.target.Value = target;
+                return TaskStatus.Success;
+            }
+                
+
+        }
+        return TaskStatus.Failure;
+    }
+
+}
---
+++ 002.Behavior Designer/project/Assets/Scripts/MySeek.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+public class MySeek : Action
+{
+    public float speed;
+    public SharedTransform target;
+    public float arriveDistance = 0.1f;
+    private float sqrArriveDistance;
+    public override void OnStart()
+    {
+        sqrArriveDistance = arriveDistance*arriveDistance;
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if (target == null || target.Value == null)
+            return TaskStatus.Failure;
+
+        transform.LookAt(target.Value.position);
+        transform.position =  Vector3.MoveTowards(transform.position, target.Value.position, speed*Time.deltaTime);
+
+        //sqrMagnitude: 求得的距离不开根号（开根号消耗性能）
+        if ((target.Value.position - transform.position).sqrMagnitude < sqrArriveDistance)
+            return TaskStatus.Success;
+
+        return TaskStatus.Running;
+
+    }
+
+}
